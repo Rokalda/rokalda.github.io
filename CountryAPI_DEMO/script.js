6a832a0090c72e8762301a69f9458b57bc38c9fe
Rokalda <nikolai.scarlett53@gmail.com>
2024-06-20 10:56:21 -0500

    Added a url query parameter for the country name, Updated to UI a little, I did the Image description, not really a big fan of it. Oh and added a Coat of arms display section.

--- CountryAPI_DEMO/script.js
+++ CountryAPI_DEMO/script.js
@@ -6,6 +6,7 @@ const restapiurl="https://restcountries.com/v3.1/"
 const  country_form = document.getElementById("country_form")
 const name_input=document.getElementById("country-names-input")
 const name_datalist = document.getElementById("country-names")
+const coa_El = document.querySelector(".country-coa")
 const c_flag = document.querySelector(".country-flag")
 const c_map = document.querySelector(".country-map")
 const searchBtn = document.getElementById("searchBtn")
@@ -13,34 +14,60 @@ const searchBtn = document.getElementById("searchBtn")
 country_form.onsubmit=(e)=>{
     console.log(e)
     e.preventDefault();
-    getCountryData()
+    displayCountryData(name_input.value)
 }
 
 
 getNames()
 
+window.onload=()=>{
+    let win_url = new URL(window.location.href)
+    if(win_url.searchParams.get("country_name")!=null){
+        displayCountryData(win_url.searchParams.get("country_name"))
+    }
+   
+    
+}
 
-async function getCountryData(){
+async function displayCountryData(country_name){
     document.querySelector(".country-data").classList.add("loading")
-    const response =await fetch(restapiurl+"name/"+name_input.value+"?fullText=True&fields=flags,name")
+    const response =await fetch(restapiurl+"name/"+country_name+"?fullText=True&fields=flags,name,latlng,coatOfArms,demonyms,population,area")
     document.querySelector(".country-data").classList.remove("loading")
 
     if(response.ok){
         console.log("Request to "+response.url+" was successful")
+
         const data = await response.json();
         let flagdata = data[0].flags
         let nameData = data[0].name;
-    
-        
+        let latlongData = data[0].latlng;
+        let coaData = data[0].coatOfArms;
+        let denonymData = data[0].demonyms;
+        let areadata = data[0].area;
+        let populationData = data[0].population;
+
         c_flag.querySelector(".title").innerHTML=`<span class="sthr_text" style="background-image:url(${flagdata.png})">${nameData.common}'s</span> FLAG`
        c_flag.querySelector("img").src=flagdata.png
        c_flag.querySelector("img").alt=flagdata.alt;
       
-       
+       if(coaData.png ==""){
+        coa_El.querySelector(".title").innerHTML=`<span class="sthr_text" style="background-image:url(${flagdata.png})">${nameData.common}</span> has no Coat of Arms`
+        coa_El.querySelector("img").src=coaData.png;
+
+       }
+       else{
+        coa_El.querySelector(".title").innerHTML=`<span class="sthr_text" style="background-image:url(${flagdata.png})">${nameData.common}'s</span> Coat of Arms`
+        coa_El.querySelector("img").src=coaData.png;
+       }
+
        c_map.querySelector(".title").innerHTML = `<span class="sthr_text" style="background-image:url(${flagdata.png})">${nameData.common}'s</span> MAP`
        c_map.querySelector("iframe").src=base_embededMapSrc +`${nameData.common}`
+        c_map.querySelector(".coords").innerHTML=`Latitude: ${latlongData[0]} <br> Longitude: ${latlongData[1]}`
+        c_map.querySelector(".area").textContent=areadata.toLocaleString();
 
-       c_flag.querySelector(".desc").innerHTML=flagdata.alt;
+        c_map.querySelector(".population").textContent=populationData.toLocaleString();
+        c_map.querySelector(".demonym").textContent=denonymData.eng.m;
+       //c_flag.querySelector(".desc").innerHTML=flagdata.alt;
     }
     else if(response.status==404){
         alert("This Country cannot be found")
@@ -51,6 +78,8 @@ async function getCountryData(){
 async function getNames(){
     const response=await fetch(restapiurl+"all?fields=name")
     const allcountrys= await response.json()
+    allcountrys.sort((a,b)=>a.name.common.localeCompare(b.name.common))
+  
    for(let country of allcountrys){
      let name = country.name;
         let option = `<option value="${name.common}"></option>`
@@ -66,6 +95,7 @@ function parseforColorNames(string){
         word=` <span style="color:${word}">${word}</span> `
         parsedString+=word
     }
+
     return parsedString
 
 }
